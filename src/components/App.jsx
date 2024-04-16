93f3046a6773a307f29caf8baf790ce2ea4679cc
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2024-04-16 19:02:29 +0300

    ex 1 almost finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import Profile from './Profile/Profile.jsx'
+import user from './Profile/user.json'
+
+
+export const App = () => 
+{return (<>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+
+    />
+</>);};
